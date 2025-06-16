99a69ef64aef6aa7e368a344d051ea2668a4b8dd
okellojohnsilas <johnsilasokello49@gmail.com>
2025-06-16 22:42:47 +0300

    Removed Preloader

--- components/front/bottom.php
+++ components/front/bottom.php
@@ -1,6 +1,6 @@
  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="far fa-circle-up"></i></a>
  <!-- Preloader -->
- <div id="preloader"></div>
+ <!-- <div id="preloader"></div> -->
  <?php 
     // include 'components/front/footer.php';
     include "components/front/scripts.php";
